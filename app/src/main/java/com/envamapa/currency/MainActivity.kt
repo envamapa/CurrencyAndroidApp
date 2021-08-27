4aeaadafad8c485a073ac9b89cde1f502dfcd13b
Enya Martínez <envamapa@gmail.com>
2021-08-27 12:26:21 -0500

    Initial commit

---
+++ app/src/main/java/com/envamapa/currency/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.envamapa.currency
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.envamapa.currency.ui.theme.CurrencyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CurrencyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CurrencyTheme {
+        Greeting("Android")
+    }
+}
